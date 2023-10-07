a42bb74208b8c5963df5784729c963f199c201f3
dariusdraco <95847901+dariusdraco@users.noreply.github.com>
2023-10-07 20:31:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia.(
+        { audio:true });
+    classifier=ml5.soundClassifier("https://storage.googleapis.com/tm-model/kA4F-80yW/model.json",modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
